980db49b93e8a433dc2dbe7bacc5a51b2cc5e82c
Gonzalo Aguado Torres <gonagutor@gmail.com>
2021-07-31 20:27:39 +0200

    Añadido boton que cambia de estado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { i: 0, liked: false };
+  }
+
+  sumarNumero = () => {
+    if (this.state.liked) this.setState({ i: this.state.i - 1, liked: false });
+    else this.setState({ i: this.state.i + 1, liked: true });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hola natalia <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={this.sumarNumero}>{this.state.i}</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
