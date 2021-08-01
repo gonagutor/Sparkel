4154f2cf4e6de417687b4ad2b7f971b2d8aef102
Gonzalo Aguado Torres <gonagutor@gmail.com>
2021-08-01 17:17:01 +0200

    Reordenado e instalado eslint

---
+++ src/pages/App.jsx
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>En desarrollo</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
+/* eslint-disable */
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../pages/App';
 
 test('renders learn react link', () => {
   render(<App />);
