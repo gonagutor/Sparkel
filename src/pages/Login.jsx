4d2ce4678555c464f8e67e63bbb2e1d42708c894
Gonzalo Aguado Torres <gonagutor@gmail.com>
2021-08-02 13:32:14 +0200

    Login form data handling. Processing not done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,24 +7,54 @@ import TopDecoration from '../res/Login/top-decoration.svg';
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+      rememberMe: false,
+    };
+
+    this.toggleValue = sessionStorage.getItem('darkThemeOn') === 'true';
+    if (this.toggleValue) Theme.changeToTheme(Theme.black);
 
     this.changeCurrentTheme = (val) => {
+      sessionStorage.setItem('darkThemeOn', val);
       if (val === true) return Theme.changeToTheme(Theme.black);
       return Theme.changeToTheme(Theme.white);
     };
 
-    this.onLogin = () => {};
+    this.formFieldsHandlers = {
+      password: (e) => {
+        this.setState({
+          password: e.target.value,
+        });
+      },
+      username: (e) => {
+        this.setState({
+          username: e.target.value,
+        });
+      },
+      rememberMe: (e) => {
+        this.setState({
+          rememberMe: e.target.checked,
+        });
+      },
+    };
+
+    this.onLogin = () => {
+      const { username, password, rememberMe } = this.state;
+      console.log(`User: ${username}; Password: ${password}; Remember me: ${rememberMe}`);
+    };
   }
 
   render() {
+    const { username, password, rememberMe } = this.state;
     return (
       <div id="container">
         <img src={TopDecoration} id="topDecoration" height="130px" width="100vw" alt="" />
         <div id="topRow">
           <h1 id="appTitle"> Sparkel </h1>
           <label htmlFor="themeChanger" className="switch">
-            <input type="checkbox" id="themeChanger" onChange={(v) => this.changeCurrentTheme(v.target.checked)} />
+            <input type="checkbox" id="themeChanger" defaultChecked={this.toggleValue} onChange={(v) => this.changeCurrentTheme(v.target.checked)} />
             <span className="slider round" />
           </label>
         </div>
@@ -37,19 +67,14 @@ class Login extends React.Component {
             </div>
           </div>
           <form>
-            <input className="inputStyle" id="usernameField" placeholder="Nombre de usuario" />
-            <input className="inputStyle" id="passwordField" type="password" placeholder="Contraseña" />
+            <input className="inputStyle" id="usernameField" value={username} onChange={this.formFieldsHandlers.username} placeholder="Nombre de usuario" />
+            <input className="inputStyle" id="passwordField" value={password} onChange={this.formFieldsHandlers.password} type="password" placeholder="Contraseña" />
             <div id="actionContainer">
               <label htmlFor="rememberme" id="remembermeLabel">
-                <input type="checkbox" id="rememberme" />
+                <input type="checkbox" id="rememberme" checked={rememberMe} onChange={this.formFieldsHandlers.rememberMe} />
                 Recuérdame
               </label>
-              <input
-                type="button"
-                id="loginButton"
-                value="Login"
-                onClick={this.onLogin}
-              />
+              <input type="button" id="loginButton" value="Login" onClick={this.onLogin} />
             </div>
           </form>
         </div>
